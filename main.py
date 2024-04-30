10a70c618a797e1648182e23a7c897e1bc75d24d
rodrigozuniga <rodrigozuniga@gmail.com>
2024-04-30 12:07:40 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 2024-04-30
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
@@ -32,7 +32,7 @@ This will install the packages from the requirements.txt for this project.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
-Bootstrap5(app)
+Bootstrap(app)
 
 # Configure Flask-Login
 login_manager = LoginManager()
